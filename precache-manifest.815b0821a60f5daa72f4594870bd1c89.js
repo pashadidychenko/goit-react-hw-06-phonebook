98cc2ec5ce99de89be6db65da6c36b4d0e0dda9f
pashadidychenko <pasha.didychenko@gmail.com>
2020-08-06 02:15:27 +0300

    Updates

---
+++ precache-manifest.815b0821a60f5daa72f4594870bd1c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a8fb3c51cb698aaa8d22610f3176ac",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "ec45fbe8479564bad101",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.36c3d190.chunk.css"
+  },
+  {
+    "revision": "72be3e8c78d9ace58fc1",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.585f86d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.585f86d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec45fbe8479564bad101",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.d3321e88.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
